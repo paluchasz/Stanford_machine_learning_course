ac28ee6cf8fe7f516d19316adea21c00f54b40e6
paluchasz <paluchasz@googlemail.com>
2018-11-19 18:12:49 +0000

    Forgot to add semicolons in the function and that's why I was getting so many unecessary printouts.

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,8 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-    gradient = 1/m * X' * (X*theta - y)
-    theta = theta - alpha * gradient
+    gradient = 1/m * X' * (X*theta - y);
+    theta = theta - alpha * gradient;
 
 
 
