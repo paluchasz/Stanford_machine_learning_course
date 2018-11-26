9871ce85865cefb9dba99dece42edb9974be3815
paluchasz <paluchasz@googlemail.com>
2018-11-26 14:42:07 +0000

    Completed the function which calculates the regularized cost J and its gradients. This only differes by an extra term.

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,8 +18,24 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+h = sigmoid(X*theta); % The hypothesis
 
+% Recall regularization term is lambda * sum of theta_j^2 but from i = 1 not 0!
+% since theta^T * theta = this sum + theta_0 ^2 we get (recall that we start
+% indexing at 1 in octave!):
+regularization_term = lambda/(2*m) * (theta' * theta - theta(1)*theta(1)); 
 
+J = 1/m * (-y' * log(h) - (1-y)' * log(1-h)) + regularization_term;
+% same as in costFunction.m plus the extra term
+
+unregularized_grad = 1/m * X' * (h - y); %unregularized vector for all gradients
+
+% Regularization term for gradient is lambda/m * theta_j for all j except j=0
+% So we want to add a vector of values lambda/m * theta_j except for first element:
+regularization_gradient_vector = lambda/m * theta;
+regularization_gradient_vector(1) = 0;
+
+grad = unregularized_grad + regularization_gradient_vector;
 
 
 % =============================================================
