3f732d30fd2a14a9b0d0ca31da9b7d7ba82406be
paluchasz <paluchasz@googlemail.com>
2018-11-26 12:45:08 +0000

    Completed the cost function to calculate the cose J and its gradient. I used the vectorised implementation. Had to call the sigmoid fct too.

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -19,8 +19,10 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
-
-
+h = sigmoid(X*theta); % The hypothesis
+% Vectorised implementation:
+J = 1/m * (-y' * log(h) - (1-y)' * log(1-h));
+grad = 1/m * X' * (h - y);
 
 
 
