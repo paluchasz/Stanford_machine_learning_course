6bf6bbadc23e3a6580389cd86a4ff255a8966392
paluchasz <paluchasz@googlemail.com>
2018-12-05 14:09:19 +0000

    This cost function is exactly identical to the one I wrote in the second assignment (since I already worked out a vectorised implementation there)

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -26,7 +26,7 @@ grad = zeros(size(theta));
 %       Each row of the resulting matrix will contain the value of the
 %       prediction for that example. You can make use of this to vectorize
 %       the cost function and gradient computations. 
-%
+
 % Hint: When computing the gradient of the regularized cost function, 
 %       there're many possible vectorized solutions, but one solution
 %       looks like:
@@ -34,19 +34,35 @@ grad = zeros(size(theta));
 %           temp = theta; 
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
-%
 
 
 
 
 
+% Just using my code from exercise 2!! Already did it vecotrized way there.
 
+h = sigmoid(X*theta); % The hypothesis
 
+% Recall regularization term is lambda * sum of theta_j^2 but from i = 1 not 0!
+% since theta^T * theta = this sum + theta_0 ^2 we get (recall that we start
+% indexing at 1 in octave!):
+regularization_term = lambda/(2*m) * (theta' * theta - theta(1)*theta(1)); 
 
+J = 1/m * (-y' * log(h) - (1-y)' * log(1-h)) + regularization_term;
+% same as in costFunction.m plus the extra term
+
+unregularized_grad = 1/m * X' * (h - y); %unregularized vector for all gradients
+
+% Regularization term for gradient is lambda/m * theta_j for all j except j=0
+% So we want to add a vector of values lambda/m * theta_j except for first element:
+regularization_gradient_vector = lambda/m * theta;
+regularization_gradient_vector(1) = 0;
+
+grad = unregularized_grad + regularization_gradient_vector;
 
 
 % =============================================================
 
-grad = grad(:);
+
 
 end
