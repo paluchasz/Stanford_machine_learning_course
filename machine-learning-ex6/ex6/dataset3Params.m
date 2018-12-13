9a75a9f8247f38ad6d48dd9dac702bf5732d7c3b
paluchasz <paluchasz@googlemail.com>
2018-12-13 13:16:35 +0000

    Completed the function dataset3Params which used the validation set to select optimum values of C and sigma.
    I used a selection of 8 different values suggested in the instructions. I used a double for loop to go through all the possible C and sigma
    in each calling other functions to: first train the model; then return a vector of predictions using the model; finally calculate the error
    on the predicted result. Then append the values of C, sigma and error to a matrix. Finally, outside the for loop just had to find the
    corresponding C and sigma for the lowest error.

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,8 +23,34 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% Values I should try are e.g 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30
+values_to_try = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+
+% initialise results which will be a matrix with rows (C_temp, sigma_temp, error)
+results = []; 
+
+for C_temp = values_to_try,
+  for sigma_temp = values_to_try,
+    
+    % Train model with temporary C and sigma values, then predict, the find the error
+    model = svmTrain(X, y, C_temp, @(x1, x2) gaussianKernel(x1, x2, sigma_temp));
+    predictions = svmPredict(model, Xval);
+    error = mean(double(predictions ~= yval));
+    
+    % add C_temp sigma_temp and error to my results matrix 
+    results = [results; C_temp sigma_temp error];
+    
+  end
+end
 
 
+min_err = min(results(:,3)); 
+% finds minimum value of 3rd column - the error
+row_of_min_err = find(results(:,3) == min_err); 
+% returns index of the row which has the min error
+
+C = results(row_of_min_err, 1);
+sigma = results(row_of_min_err, 2);
 
 
 
